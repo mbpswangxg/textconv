43837559976dd473de8295ebe539de5732bf6743
王 卓 <wangz@ad.intra.intra-mart.jp>
2021-02-22 13:23:59 +0900

    PORTALルール修正

--- bin/textconv.bat
+++ bin/textconv.bat
@@ -2,5 +2,5 @@ cd C:\workspace\textconv\bin
 
 set P1=C:\workspace\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0019_conengplan\csp\
 
-textconv -c delcoimart -d %P1%
+textconv -c PORTAL -d %P1%
 
