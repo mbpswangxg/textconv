2d6d2de1dfa9cf70ad06c9837d4ce73b479e711e
王 卓 <wangz@ad.intra.intra-mart.jp>
2021-03-05 20:38:36 +0900

    commit　追加

--- bin/textconv.bat
+++ bin/textconv.bat
@@ -3,8 +3,8 @@ cd C:\workspace\textconv\bin
 set P1=C:\resin\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0020_conmnfequip\equipinspect\
 set P1=C:\resin\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0010_conbsnplan\equipchk
 set P1=C:\tmp\wangx\_projects\jae\develop\jae\jae\src\main\jssp\src\jae\0010_conbsnplan\equipchk\
-set P1=C:\resin\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0019_conengplan\csp
+set P1=C:\workspace\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0019_conengplan\csp\res\
 
-REM TextConv.exe -c delcoimart -d %P1%
+TextConv.exe -c clearlineaddreturn -d %P1%
 
-textconv -x %P1%
+REM textconv -x %P1%        
