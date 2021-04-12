b9a3ab7d91b1d38f7bcdc8fcfdef30e700865eff
wangx <wangx@intra-mart.jp>
2021-04-12 13:49:11 +0900

    update conv

--- bin/textconv.bat
+++ bin/textconv.bat
@@ -1,5 +1,5 @@
 REM set P1=C:\resin\resin-pro-4.0.64\webapps\imart\WEB-INF\jssp\src\jae\0010_conbsnplan\pricechg
-set P1="C:\tool\textconv\bin\webyml\1800_PurReq_CommMgmt.yml"
+set P1="C:\tool\textconv\bin\webyml\1800_SampleTransfer_Smp-Reg1.yml"
 REM set P1=C:\tmp\wangx\_projects\jae\develop\jae\jae\src\main\jssp\src\jae\0000_common\proposal\
 cd C:\tool\textconv\bin
 REM textconv -x xpath -d %P1%
